0383106ae1bffc2575c2a1ef7a021685e05bac98
Andrew Arnott <andrew.arnott@microsoft.com>
2026-07-03 16:43:27 -0600

    Fix NativeAOT test failures

--- test/Nerdbank.Json.Tests/JsonSerializerTests.cs
+++ test/Nerdbank.Json.Tests/JsonSerializerTests.cs
@@ -5,7 +5,6 @@ using System.Drawing;
 using System.Globalization;
 using System.Numerics;
 using System.Text;
-using PolyType.Abstractions;
 
 [GenerateShapeFor<char>]
 [GenerateShapeFor<byte>]
@@ -47,27 +46,27 @@ public partial class JsonSerializerTests : TestBase
 {
 	[Test]
 	public void Serialize_StringUsesRfc8259EscapingRules()
-		=> this.AssertRoundtrip(
+		=> this.AssertRoundtrip<string, JsonSerializerTests>(
 			"ü <tag> \"quoted\" \\ slash\nnext",
 			"\"ü <tag> \\\"quoted\\\" \\\\ slash\\nnext\"");
 
 	[Test]
 	public void SerializeDeserialize_PrimitiveScalars()
 	{
-		this.AssertRoundtrip('A', "\"A\"");
-		this.AssertRoundtrip((byte)42, "42");
-		this.AssertRoundtrip((sbyte)-42, "-42");
-		this.AssertRoundtrip((short)-1234, "-1234");
-		this.AssertRoundtrip((ushort)65530, "65530");
-		this.AssertRoundtrip(123456, "123456");
-		this.AssertRoundtrip(4000000000u, "4000000000");
-		this.AssertRoundtrip(-9876543210L, "-9876543210");
-		this.AssertRoundtrip(18446744073709551610UL, "18446744073709551610");
-		this.AssertRoundtrip(true, "true");
-		this.AssertRoundtrip(false, "false");
-		this.AssertRoundtrip(1.5f, "1.5");
-		this.AssertRoundtrip(1.5d, "1.5");
-		this.AssertRoundtrip(79228162514264337593543950335m, "79228162514264337593543950335");
+		this.AssertRoundtrip<char, JsonSerializerTests>('A', "\"A\"");
+		this.AssertRoundtrip<byte, JsonSerializerTests>((byte)42, "42");
+		this.AssertRoundtrip<sbyte, JsonSerializerTests>((sbyte)-42, "-42");
+		this.AssertRoundtrip<short, JsonSerializerTests>((short)-1234, "-1234");
+		this.AssertRoundtrip<ushort, JsonSerializerTests>((ushort)65530, "65530");
+		this.AssertRoundtrip<int, JsonSerializerTests>(123456, "123456");
+		this.AssertRoundtrip<uint, JsonSerializerTests>(4000000000u, "4000000000");
+		this.AssertRoundtrip<long, JsonSerializerTests>(-9876543210L, "-9876543210");
+		this.AssertRoundtrip<ulong, JsonSerializerTests>(18446744073709551610UL, "18446744073709551610");
+		this.AssertRoundtrip<bool, JsonSerializerTests>(true, "true");
+		this.AssertRoundtrip<bool, JsonSerializerTests>(false, "false");
+		this.AssertRoundtrip<float, JsonSerializerTests>(1.5f, "1.5");
+		this.AssertRoundtrip<double, JsonSerializerTests>(1.5d, "1.5");
+		this.AssertRoundtrip<decimal, JsonSerializerTests>(79228162514264337593543950335m, "79228162514264337593543950335");
 	}
 
 	[Test]
@@ -86,53 +85,59 @@ public partial class JsonSerializerTests : TestBase
 			cultureJson = "\"\"";
 		}
 
-		this.AssertRoundtripBclComparer(new BigInteger(1234567890123456789L), "1234567890123456789");
-		this.AssertRoundtripBclComparer(
+		this.AssertRoundtrip<BigInteger, JsonSerializerTests>(new BigInteger(1234567890123456789L), "1234567890123456789", EqualityComparer<BigInteger>.Default);
+		this.AssertRoundtrip<DateTime, JsonSerializerTests>(
 			new DateTime(2026, 6, 25, 18, 17, 16, 123, DateTimeKind.Utc).AddTicks(4567),
-			"\"2026-06-25T18:17:16.1234567Z\"");
-		this.AssertRoundtripBclComparer(
+			"\"2026-06-25T18:17:16.1234567Z\"",
+			EqualityComparer<DateTime>.Default);
+		this.AssertRoundtrip<DateTimeOffset, JsonSerializerTests>(
 			new DateTimeOffset(2026, 6, 25, 18, 17, 16, 123, TimeSpan.FromHours(-7)).AddTicks(4567),
-			"\"2026-06-25T18:17:16.1234567-07:00\"");
-		this.AssertRoundtripBclComparer(
+			"\"2026-06-25T18:17:16.1234567-07:00\"",
+			EqualityComparer<DateTimeOffset>.Default);
+		this.AssertRoundtrip<TimeSpan, JsonSerializerTests>(
 			new TimeSpan(1, 2, 3, 4, 5).Add(TimeSpan.FromTicks(6)),
-			"\"1.02:03:04.0050006\"");
-		this.AssertRoundtripBclComparer(
+			"\"1.02:03:04.0050006\"",
+			EqualityComparer<TimeSpan>.Default);
+		this.AssertRoundtrip<Guid, JsonSerializerTests>(
 			Guid.Parse("01234567-89ab-cdef-0123-456789abcdef"),
-			"\"01234567-89ab-cdef-0123-456789abcdef\"");
-		this.AssertRoundtripBclComparer(
+			"\"01234567-89ab-cdef-0123-456789abcdef\"",
+			EqualityComparer<Guid>.Default);
+		this.AssertRoundtrip<Version, JsonSerializerTests>(
 			new Version(1, 2, 3, 4),
-			"\"1.2.3.4\"");
-		this.AssertRoundtripBclComparer(
+			"\"1.2.3.4\"",
+			EqualityComparer<Version>.Default);
+		this.AssertRoundtrip<Uri, JsonSerializerTests>(
 			new Uri("https://example.com/a?b=c", UriKind.Absolute),
-			"\"https://example.com/a?b=c\"");
-		this.AssertRoundtripBclComparer(culture, cultureJson);
-		this.AssertRoundtripBclComparer(Encoding.UTF8, "\"utf-8\"");
-		this.AssertRoundtripBclComparer(Color.FromArgb(unchecked((int)0xFF336699)), "-13408615");
-		this.AssertRoundtripBclComparer(new Point(12, -34), "[12,-34]");
+			"\"https://example.com/a?b=c\"",
+			EqualityComparer<Uri>.Default);
+		this.AssertRoundtrip<CultureInfo, JsonSerializerTests>(culture, cultureJson, EqualityComparer<CultureInfo>.Default);
+		this.AssertRoundtrip<Encoding, JsonSerializerTests>(Encoding.UTF8, "\"utf-8\"", EqualityComparer<Encoding>.Default);
+		this.AssertRoundtrip<Color, JsonSerializerTests>(Color.FromArgb(unchecked((int)0xFF336699)), "-13408615", EqualityComparer<Color>.Default);
+		this.AssertRoundtrip<Point, JsonSerializerTests>(new Point(12, -34), "[12,-34]", EqualityComparer<Point>.Default);
 	}
 
 	[Test]
 	public void SerializeDeserialize_ByteBuffers()
 	{
-		this.AssertRoundtrip(new byte[] { 1, 2, 3, 4 }, "\"AQIDBA==\"");
-		this.AssertRoundtrip(new Memory<byte>([5, 6, 7]), "\"BQYH\"");
-		this.AssertRoundtrip(new ReadOnlyMemory<byte>([8, 9, 10]), "\"CAkK\"");
+		this.AssertRoundtrip<byte[], JsonSerializerTests>(new byte[] { 1, 2, 3, 4 }, "\"AQIDBA==\"");
+		this.AssertRoundtrip<Memory<byte>, JsonSerializerTests>(new Memory<byte>(new byte[] { 5, 6, 7 }), "\"BQYH\"");
+		this.AssertRoundtrip<ReadOnlyMemory<byte>, JsonSerializerTests>(new ReadOnlyMemory<byte>(new byte[] { 8, 9, 10 }), "\"CAkK\"");
 	}
 
 #if NET8_0_OR_GREATER
 	[Test]
 	public void SerializeDeserialize_ModernBuiltInTypes()
 	{
-		this.AssertRoundtrip((Half)1.5f, "1.5");
-		this.AssertRoundtrip(Int128.Parse("170141183460469231731687303715884105727", CultureInfo.InvariantCulture), "170141183460469231731687303715884105727");
-		this.AssertRoundtrip(UInt128.Parse("340282366920938463463374607431768211455", CultureInfo.InvariantCulture), "340282366920938463463374607431768211455");
-		this.AssertRoundtrip(
+		this.AssertRoundtrip<Half, JsonSerializerTests>((Half)1.5f, "1.5");
+		this.AssertRoundtrip<Int128, JsonSerializerTests>(Int128.Parse("170141183460469231731687303715884105727", CultureInfo.InvariantCulture), "170141183460469231731687303715884105727");
+		this.AssertRoundtrip<UInt128, JsonSerializerTests>(UInt128.Parse("340282366920938463463374607431768211455", CultureInfo.InvariantCulture), "340282366920938463463374607431768211455");
+		this.AssertRoundtrip<DateOnly, JsonSerializerTests>(
 			DateOnly.ParseExact("2026-06-25", "yyyy-MM-dd", CultureInfo.InvariantCulture),
 			"\"2026-06-25\"");
-		this.AssertRoundtrip(
+		this.AssertRoundtrip<TimeOnly, JsonSerializerTests>(
 			TimeOnly.ParseExact("18:17:16.1234567", "HH:mm:ss.fffffff", CultureInfo.InvariantCulture),
 			"\"18:17:16.1234567\"");
-		this.AssertRoundtrip(
+		this.AssertRoundtrip<Rune, JsonSerializerTests>(
 			new Rune(0x1F98A),
 			"\"🦊\"");
 	}
@@ -188,10 +193,4 @@ public partial class JsonSerializerTests : TestBase
 
 		Assert.Throws<FormatException>(() => serializer.Deserialize<Point, JsonSerializerTests>("""[/* x */12,-34]"""));
 	}
-
-	private void AssertRoundtrip<T>(T value, string expectedJson)
-		=> this.AssertRoundtrip(value, TypeShapeResolver.ResolveDynamic<T>() ?? TypeShapeResolver.ResolveDynamicOrThrow<T, JsonSerializerTests>(), expectedJson);
-
-	private void AssertRoundtripBclComparer<T>(T value, string expectedJson)
-		=> this.AssertRoundtrip(value, TypeShapeResolver.ResolveDynamic<T>() ?? TypeShapeResolver.ResolveDynamicOrThrow<T, JsonSerializerTests>(), expectedJson, EqualityComparer<T>.Default);
 }
